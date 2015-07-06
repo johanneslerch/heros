173bbbdfaf101d94c9e4a27c8dfd18752bbb8724
Johannes Lerch <lerch.johannes@googlemail.com>
2015-07-06 09:38:11 +0200

    switching to concrete class and maven project

--- src/heros/ide/edgefunc/fieldsens/AccessPathBundle.java
+++ src/heros/ide/edgefunc/fieldsens/AccessPathBundle.java
@@ -2,15 +2,69 @@ package heros.ide.edgefunc.fieldsens;
 
 import java.util.Set;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
 
+import fj.data.List;
 
-public interface AccessPathBundle<Field> {
+public class AccessPathBundle<FieldAccess> {
 
-	AccessPathBundle<Field> prepend(Field field);
+	private Set<List<FieldAccess>> paths = Sets.newHashSet();
+
+	public AccessPathBundle() {
+		paths.add(List.<FieldAccess> nil());
+	}
+
+	private AccessPathBundle(Set<List<FieldAccess>> paths) {
+		this.paths = paths;
+	}
+
+	public Set<List<FieldAccess>> getPaths() {
+		return paths;
+	}
+
+	public AccessPathBundle<FieldAccess> join(AccessPathBundle<FieldAccess> right) {
+		Set<List<FieldAccess>> result = Sets.newHashSet(paths);
+		result.addAll(right.paths);
+		return new AccessPathBundle<FieldAccess>(result);
+	}
+
+	public AccessPathBundle<FieldAccess> prepend(FieldAccess field) {
+		Set<List<FieldAccess>> result = Sets.newHashSet();
+		for (List<FieldAccess> path : paths) {
+			result.add(path.cons(field));
+		}
+		return new AccessPathBundle<FieldAccess>(result);
+	}
+
+	public AccessPathBundle<FieldAccess> read(FieldAccess field) {
+		Set<List<FieldAccess>> result = Sets.newHashSet();
+		for (List<FieldAccess> path : paths) {
+			if (path.isNotEmpty() && path.head().equals(field))
+				result.add(path.tail());
+		}
+		return result.isEmpty() ? null : new AccessPathBundle<FieldAccess>(result);
+	}
+
+	public AccessPathBundle<FieldAccess> overwrite(Set<FieldAccess> fields) {
+		Set<List<FieldAccess>> result = Sets.newHashSet();
+		for (List<FieldAccess> path : paths) {
+			if (path.isEmpty() || !fields.contains(path.head()))
+				result.add(path);
+		}
+		return result.isEmpty() ? null : new AccessPathBundle<FieldAccess>(result);
+	}
+
+	@Override
+	public String toString() {
+		return "{" + Joiner.on(", ").join(Iterables.transform(paths, new Function<List<FieldAccess>, String>() {
+			@Override
+			public String apply(List<FieldAccess> input) {
+				return Joiner.on(".").join(input);
+			}
+		})) + "}";
+	}
 	
-	AccessPathBundle<Field> overwrite(Set<Field> field);
-	
-	AccessPathBundle<Field> read(Field field);
-	
-	AccessPathBundle<Field> join(AccessPathBundle<Field> bundle);
 }
